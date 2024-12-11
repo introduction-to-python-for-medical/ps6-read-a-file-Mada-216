060b60d79e544a7f3577e94edfc688d930baca23
Mada-216 <hamada.odeh19@gmail.com>
2024-12-12 01:11:44 +0200

    Update codons.py

--- codons.py
+++ codons.py
@@ -1,14 +1,10 @@
-def create_codon_dict(path):
+def create_codon_dict(file_path):
+   
     codon_dict = {}
     with open(path, 'r') as file: 
-        for row in file:
-            # Split the row by tab characters
-            cells = row.strip().split('\t')
-            if len(cells) >= 3:  # Check that there are enough columns
-                codon = cells[0]
-                amino_acid = cells[2]
-                codon_dict[codon] = amino_acid
-            else:
-                # Handle rows that do not have enough columns if needed
-                print(f"Skipping malformed row: {row}")
-    return codon_dic
+         for row in file:
+             cells = row.strip().split('ََ\t')
+             codon = cells [0]
+             amino_acid = cells[2]
+             codon_dict[codon] = amino_acid
+    return codon_dict
